d3bbeaa04030c86b276004474175e73b3618c155
User511A <48769117+User511A@users.noreply.github.com>
2022-01-22 19:30:36 +0500

    -

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
+#https://docs-python.ru/standart-library/modul-platform-python/
+#https://cryptoworld.su/sbor-informacii-o-sisteme-s-python/
 from flask import Flask
+import platform
+import getpass
+import os
+import socket
+import psutil
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello! I am running in a docker container' 
+    info = (platform.platform(), platform.processor())
+    return info[1]
 
 if __name__ == '__main__': 
     app.run(host='0.0.0.0', port=5000)
